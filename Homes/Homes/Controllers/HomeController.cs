158925c9665e59f6e7f2dd48fe29f3cff14204d3
Gashaw1 <akalc2004@yahoo.com>
2018-07-18 14:55:14 -0400

    .

---
+++ Homes/Homes/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Homes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homes.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Search()
+        {
+            
+            var houses = new List<Addresse>();
+            houses = Addresse.HouseReturnAll();
+
+            return PartialView("_menu", houses);
+        }
+        [HttpPost]
+        public ActionResult Search(string q)
+        {
+            string m = q;
+            var houses = new List<Addresse>();
+            houses = Addresse.HouseReturnAll();
+
+            return PartialView("_menu", houses);
+        }
+
+    }
+}
---
+++ Homes/Homes/Models/Home.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Homes.Models
+{
+    public class Home
+    {
+    }
+    public class Addresse
+    {
+        public Addresse()
+        {
+        }
+
+        string ID { get; set; }
+        public string state { get; set; }
+        public string city { get; set; }
+        public string ZipCode { get; set; }
+        public string _date { get; set; }
+        public string userID { get; set; }
+        public HouseImage HouseImage { get; set; }
+        public Description Description { get; set; }
+
+        public static List<Addresse> HouseReturnAll()
+        {
+            var houses = new List<Addresse>();
+
+            var house = new Addresse();
+            house.city = "Atlanta";
+            house.state = "Georgia";
+            house.ZipCode = "303229";
+            house.HouseImage = new HouseImage()
+            {
+                ImageNmae = "capture22.png"
+            };
+            house.Description = new Description()
+            {
+                desctiption = "locatiod in addwethtermod this is locatiod in addwethtermod this is"
+            };
+
+            var house2 = new Addresse();
+            house2.city = "Brookhaven";
+            house2.state = "Georgia";
+            house.ZipCode = "32229";
+            house2.HouseImage = new HouseImage()
+            {
+                ImageNmae = "capture23.png"
+            };
+            house2.Description = new Description()
+            {
+                desctiption = "This is a good place to live halaf desctipiton sample adndoss"
+            };
+
+            houses.Add(house);
+            houses.Add(house2);
+
+            return houses;
+        }
+    }
+    
+    public class HouseImage
+    {
+        string ID { get; set; }
+        public string ImageNmae { get; set; }
+        string House_ID { get; set; }
+    }
+    public class Description
+    {
+        string ID { get; set; }
+        public string desctiption { get; set; }
+        string House_ID { get; set; }
+    }
+    public class Restriction
+    {
+        string ID { get; set; }
+        public string restriction { get; set; }
+        string House_ID { get; set; }
+
+    }
+    public class Contact
+    {
+        string ID { get; set; }
+        string Phone { get; set; }
+        string Email { get; set; }
+        string House_ID { get; set; }
+
+    }
+}
